e2c3aac11031f461c190a590c76da7175ba0ca4d
BIBI-HAJIRA3 <bibihajirasanu@gmail.com>
2026-04-07 18:42:38 +0530

    Update Service.js

--- models/Service.js
+++ models/Service.js
@@ -8,18 +8,24 @@ const ServiceSchema = new mongoose.Schema(
     city: { type: String, required: true, trim: true },
     pincode: { type: String, required: true, trim: true },
     address: { type: String, required: true, trim: true },
+
     imagePath: { type: String, default: '' },
     providerImages: [{ type: String }],
+
     location: {
       lat: { type: Number },
       lng: { type: Number },
     },
+
     averageRating: { type: Number, default: 0 },
     ratingCount: { type: Number, default: 0 },
     reviewCount: { type: Number, default: 0 },
+
     isApproved: { type: Boolean, default: true },
+
     deleteCode: { type: String, required: false },
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+
     removalRequested: { type: Boolean, default: false },
     removalRequestedBy: {
       type: mongoose.Schema.Types.ObjectId,
